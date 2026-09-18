a57f7eee0fbd4195988f9081830ff3b38652f453
Slothsoft <admin@slothsoft.de>
2026-09-18 20:11:46 +0200

    Remove redundant achievements switch

--- source/Start-ScrapMechanic.cmd
+++ source/Start-ScrapMechanic.cmd
@@ -1,8 +1,7 @@
 @echo off
 
 powershell.exe -NoProfile -ExecutionPolicy Bypass ^
-  -File "%~dp0Bootstrapper.ps1" ^
-  -EnableDevAchievements
+  -File "%~dp0Bootstrapper.ps1"
 
 if errorlevel 1 (
     pause
