561c1208aa0b1b4da3eb9e6d0a19216a32d2d0dd
Slothsoft <admin@slothsoft.de>
2026-09-18 20:00:05 +0200

    Forward game command with dev achievements enabled

--- source/Start-ScrapMechanic.cmd
+++ source/Start-ScrapMechanic.cmd
@@ -1,3 +1,7 @@
 @echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Bootstrapper.ps1"
+if "%~1"=="" (
+    powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Bootstrapper.ps1" -EnableDevAchievements
+) else (
+    powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Bootstrapper.ps1" -EnableDevAchievements -GameCommand %*
+)
 if errorlevel 1 pause
